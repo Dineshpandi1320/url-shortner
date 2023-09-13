e86cd7a2b6dc5d1aaecab78a90fbf794ba737a74
Dineshpandi1320 <dineshd1307@gmail.com>
2023-09-13 15:50:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import {Routes, Route, useNavigate } from "react-router-dom";
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import { Signup } from './Signup';
+import { Home } from './Home';
+import { Login } from './Login';
+import { ProductedRoute } from './ProductedRoute';
+import { Forgot } from './Forgot';
+import { Reset } from './Reset';
+import { Fullurl } from './Fullurl';
+
+export default function App() {
+  const navigate=useNavigate()
+   return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+
+          <Button onClick={()=>navigate("/")} color="inherit">HOME</Button>
+          {/* <Button onClick={()=>navigate("/url-short")} color="inherit">URL-SHORTENER</Button> */}
+          <Button sx={{marginLeft:"auto"}}  onClick={()=>navigate("/signup")} color="inherit">SIGNUP</Button>
+          <Button onClick={()=>navigate("/login")} color="inherit">LOGIN</Button>
+          {/* <Button onClick={()=>navigate("/reset")} color="inherit">reset</Button> */}
+         
+
+        </Toolbar>
+      </AppBar>
+       <Routes>
+        
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/url-short" element={ <Url/></ProductedRoute>} /> */}
+          <Route path="/signup" element={ <Signup/>} />
+          <Route path="/login" element={ <Login/>} />
+          <Route path="/forgot" element={ <Forgot/>} />
+          <Route path="/reset" element={ <Reset/>} />
+          <Route path="/fullurl" element={<ProductedRoute><Fullurl/></ProductedRoute> } />
+          
+       
+      </Routes>
+      
+     
+     
     </div>
-  );
+  )
 }
 
-export default App;
+
