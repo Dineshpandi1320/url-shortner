bb6c1fe2a9b63a51906026c6259d07de22391334
Dineshpandi1320 <dineshd1307@gmail.com>
2023-09-20 15:33:21 +0530

    second

--- src/Fullurl.js
+++ src/Fullurl.js
@@ -21,15 +21,7 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
   },
 }));
 
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
-    backgroundColor: theme.palette.action.hover,
-  },
-  // hide last border
-  '&:last-child td, &:last-child th': {
-    border: 0,
-  },
-}));
+
 export function Fullurl() {
   const[loader,setLoader]=useState("false")
   const[input,setInput]=useState("")
